f45787a280c57b0f9eb1d0631f08063542a8cbe8
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-11-02 12:24:40 +0100

    CREATE AN EMPTY BOARD AND FUNCTION DRAWBOARD

--- tetris.js
+++ tetris.js
@@ -1,12 +1,33 @@
 const cvs = document.getElementById("tetris")
 const ctx = cvs.getContext("2d")
-const SQ = 20
+const sq = 20
 
 function drawSquare (x,y,color) {
     ctx.fillStyle = color
-    ctx.fillRect(x*SQ,y*SQ,SQ,SQ)
+    ctx.fillRect(x*sq,y*sq,sq,sq)
     ctx.strokeStyle = "black"
-    ctx.strokeRect(x*SQ,y*SQ,SQ,SQ)
+    ctx.strokeRect(x*sq,y*sq,sq,sq)
 }
 
-drawSquare(2,1,"blue")
+
+const row = 20
+const column = 10
+const vacant = "white"
+let board = []
+
+for (let r=0; r<row; r++){
+    board[r] = []
+    for (let c=0; c<column; c++){
+        board[r][c] = vacant
+    }
+}
+
+function drawBoard(){
+    for (let r=0; r<row; r++){
+        for (let c=0; c<column; c++){
+            drawSquare(c,r,board[r][c])
+        }
+    }
+}
+
+drawBoard()
